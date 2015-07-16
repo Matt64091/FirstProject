eadd1d7207d6aa57ed730f7b1f2da3601409f213
Matt64091 <m.martell58@yahoo.com>
2015-07-16 18:36:17 -0400

    First version. Contains Q1 and 2.

---
+++ First Project w Questions/src/Methods.java
@@ -0,0 +1,24 @@
+
+public class Methods {
+	void Q1(char answer) {
+		if(answer == 'y') System.out.println("I should have figured that. I am the most famous squirrel there is.");
+		else if(answer == 'n') System.out.println("Well fuck you. I'm only the most famous squirrel around. All the babes know me.");
+		else return;
+	}
+	boolean isValid(char ch) {
+		if(ch == 'y' || ch == 'n') return true;
+		else return false;
+	}
+	boolean isValid2(char ch) {
+		if(ch >= '1' && ch <= '9') return true;
+		else return false;
+	}
+	void Q2(char answer) {
+		if(answer == '1') System.out.println("Well fuck you then");
+		else if(answer > '1' && answer <'4') System.out.println("You should try some then.");
+		else if(answer > '3' && answer < '7') System.out.println("I have some nuts you could try, but I found them in monkey shit.");
+		else if(answer > '7' && answer < '8') System.out.println("Since you like them so much take these. I bought them at the grocery store.");
+		else if(answer == 9) System.out.println("You can suck on mine if you want.");
+		else System.out.println("I said 1-9 you dumb fuck!");
+	}
+}
---
+++ First Project w Questions/src/Run.java
@@ -0,0 +1,34 @@
+
+public class Run {
+	public static void main(String args[])
+			throws java.io.IOException{
+			char answer, ignore;
+			Methods question = new Methods();
+			
+			System.out.println("Hello! Do you know who I am?");
+			System.out.println("Please reply with either y for yes or n for no:");
+			
+			for(;;) {
+				do {
+					answer = (char) System.in.read();
+					
+					do {
+						ignore = (char) System.in.read();
+					}while (ignore != '\n');
+				}while (!question.isValid(answer));
+				question.Q1(answer);
+				
+				System.out.println("On a scale of 1-9, how much do you like nuts?");
+				
+				for(;;) {
+					do {
+						answer = (char) System.in.read();
+						do {
+							ignore = (char) System.in.read();
+						}while (ignore != '\n');
+					}while (!question.isValid2(answer));
+					question.Q2(answer);
+				}
+			}
+		}
+}
