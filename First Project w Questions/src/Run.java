a9ccd75b804230f7b14518688629f4b767db9a21
Matt64091 <m.martell58@yahoo.com>
2015-07-16 20:34:17 -0400

    Fixed error. Infinite for loop = bad

--- First Project w Questions/src/Run.java
+++ First Project w Questions/src/Run.java
@@ -8,7 +8,6 @@ public class Run {
 			System.out.println("Hello! Do you know who I am?");
 			System.out.println("Please reply with either y for yes or n for no:");
 			
-			for(;;) {
 				do {
 					answer = (char) System.in.read();
 					
@@ -20,7 +19,6 @@ public class Run {
 				
 				System.out.println("On a scale of 1-9, how much do you like nuts?");
 				
-				for(;;) {
 					do {
 						answer = (char) System.in.read();
 						do {
@@ -28,20 +26,16 @@ public class Run {
 						}while (ignore != '\n');
 					}while (!question.isValid2(answer));
 					question.Q2(answer);
-				}
 				if (answer != '1')
 				{
 					System.out.println("Do you want some of these nuts?");
-					for(;;) {
 						do {
 							answer = (char) System.in.read();
 							do {
 								ignore = (char) System.in.read();
 							}while (ignore != '\n');
-						}while (!question.isValid2(answer));
+						}while (!question.isValid(answer));
 						question.Q3(answer);
 				}
 		}
-}
-}
 }
