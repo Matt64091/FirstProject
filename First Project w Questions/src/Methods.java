459d7403dab6eb498bff05acd68207ff9a65c8fa
Matt64091 <m.martell58@yahoo.com>
2015-08-16 16:15:48 -0400

    Cleaned up code, started point system(not currently active).

--- First Project w Questions/src/Methods.java
+++ First Project w Questions/src/Methods.java
@@ -1,31 +1,16 @@
 
 public class Methods {
 	int randomarray[] = new int[19];
-	int random = 0;
+	int random = 0, qvalue;
+	char rightanswer;
 	void Q1(char answer) {
 		if(answer == 'y') System.out.println("I should have figured that. I am the most famous squirrel there is.");
 		else if(answer == 'n') System.out.println("Well fuck you. I'm only the most famous squirrel around. All the babes know me.");
 		else return;
 	}
-	boolean isValid(char ch) {
-		if(ch == 'y' || ch == 'n') return true;
-		else return false;
-	}
-	boolean isValid2(char ch) {
-		if(ch >= '1' && ch <= '9') return true;
-		else return false;
-	}
-	void Q2(char answer) {
-		if(answer == '1') System.out.println("Well fuck you then");
-		else if(answer > '1' && answer <'4') System.out.println("You should try some then.");
-		else if(answer > '3' && answer < '7') System.out.println("I have some nuts you could try, but I found them in monkey shit.");
-		else if(answer > '6' && answer < '9') System.out.println("Since you like them so much take these. I bought them at the grocery store.");
-		else if(answer == '9') System.out.println("You can suck on mine if you want.");
-		else System.out.println("I said 1-9 you dumb fuck!");
-	}
-	void Q3(char answer) {
-		if(answer == 'y') System.out.println("Here you go, enjoy them!");
-		else System.out.println("Fuck you, I'll make you suck on them!");
+	int isValid(char answer) {
+		if(answer == rightanswer) return qvalue;
+		else return 0;
 	}
 	int randommeth(){
 		do{
@@ -37,64 +22,104 @@ public class Methods {
 	void choice(int choice){
 		switch(choice){
 		case 0:
-			System.out.println("Q" + choice);
+			System.out.println("Q" + choice + ":...");
+			rightanswer = 'y';
+			qvalue = 100;
 			break;
 		case 1:
-			System.out.println("Q" + choice);
+			System.out.println("Q" + choice + ":...");
+			rightanswer = 'y';
+			qvalue = 100;
 			break;
 		case 2:
-			System.out.println("Q" + choice);
+			System.out.println("Q" + choice + ":...");
+			rightanswer = 'y';
+			qvalue = 100;
 			break;
 		case 3:
-			System.out.println("Q" + choice);
+			System.out.println("Q" + choice + ":...");
+			rightanswer = 'y';
+			qvalue = 100;
 			break;
 		case 4:
-			System.out.println("Q" + choice);
+			System.out.println("Q" + choice + ":...");
+			rightanswer = 'y';
+			qvalue = 100;
 			break;
 		case 5:
-			System.out.println("Q" + choice);
+			System.out.println("Q" + choice + ":...");
+			rightanswer = 'y';
+			qvalue = 100;
 			break;
 		case 6:
-			System.out.println("Q" + choice);
+			System.out.println("Q" + choice + ":...");
+			rightanswer = 'y';
+			qvalue = 100;
 			break;
 		case 7:
-			System.out.println("Q" + choice);
+			System.out.println("Q" + choice + ":...");
+			rightanswer = 'y';
+			qvalue = 100;
 			break;
 		case 8:
-			System.out.println("Q" + choice);
+			System.out.println("Q" + choice + ":...");
+			rightanswer = 'y';
+			qvalue = 100;
 			break;
 		case 9:
-			System.out.println("Q" + choice);
+			System.out.println("Q" + choice + ":...");
+			rightanswer = 'y';
+			qvalue = 100;
 			break;
 		case 10:
-			System.out.println("Q" + choice);
+			System.out.println("Q" + choice + ":...");
+			rightanswer = 'y';
+			qvalue = 100;
 			break;
 		case 11:
-			System.out.println("Q" + choice);
+			System.out.println("Q" + choice + ":...");
+			rightanswer = 'y';
+			qvalue = 100;
 			break;
 		case 12:
-			System.out.println("Q" + choice);
+			System.out.println("Q" + choice + ":...");
+			rightanswer = 'y';
+			qvalue = 100;
 			break;
 		case 13:
-			System.out.println("Q" + choice);
+			System.out.println("Q" + choice + ":...");
+			rightanswer = 'y';
+			qvalue = 100;
 			break;
 		case 14:
-			System.out.println("Q" + choice);
+			System.out.println("Q" + choice + ":...");
+			rightanswer = 'y';
+			qvalue = 100;
 			break;
 		case 15:
-			System.out.println("Q" + choice);
+			System.out.println("Q" + choice + ":...");
+			rightanswer = 'y';
+			qvalue = 100;
 			break;
 		case 16:
-			System.out.println("Q" + choice);
+			System.out.println("Q" + choice + ":...");
+			rightanswer = 'y';
+			qvalue = 100;
 			break;
 		case 17:
-			System.out.println("Q" + choice);
+			System.out.println("Q" + choice + ":...");
+			rightanswer = 'y';
+			qvalue = 100;
 			break;
 		case 18:
-			System.out.println("Q" + choice);
+			System.out.println("Q" + choice + ":...");
+			rightanswer = 'y';
+			qvalue = 100;
 			break;
 		case 19:
-			System.out.println("Q" + choice);
+			System.out.println("Q" + choice + ":...");
+			rightanswer = 'y';
+			qvalue = 100;
 			break;
 	}
 	}
--- First Project w Questions/src/Run.java
+++ First Project w Questions/src/Run.java
@@ -5,41 +5,8 @@ public class Run {
 			char answer, ignore;
 			int random;
 			Methods question = new Methods();
-			for(int t=0; t<19; t++){
+			System.out.println("Welcome to trivia! Pick your subject:(coming soon");
 			random = question.randommeth();
 			question.choice(random);
-			}
-			System.out.println("Hello! Do you know who I am?");
-			System.out.println("Please reply with either y for yes or n for no:");
-			
-				do {
-					answer = (char) System.in.read();
-					
-					do {
-						ignore = (char) System.in.read();
-					}while (ignore != '\n');
-				}while (!question.isValid(answer));
-				question.Q1(answer);
-				
-				System.out.println("On a scale of 1-9, how much do you like nuts?");
-				
-					do {
-						answer = (char) System.in.read();
-						do {
-							ignore = (char) System.in.read();
-						}while (ignore != '\n');
-					}while (!question.isValid2(answer));
-					question.Q2(answer);
-				if (answer != '1')
-				{
-					System.out.println("Do you want some of these nuts?");
-						do {
-							answer = (char) System.in.read();
-							do {
-								ignore = (char) System.in.read();
-							}while (ignore != '\n');
-						}while (!question.isValid(answer));
-						question.Q3(answer);
-				}
 		}
 }
