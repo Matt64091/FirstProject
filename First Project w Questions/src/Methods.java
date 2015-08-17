b789039aff0276a4cc756e64b4f9c79136c25a85
Matt64091 <m.martell58@yahoo.com>
2015-08-17 14:43:33 -0400

    Added choice inputs for point values and subjects.

--- First Project w Questions/src/Methods.java
+++ First Project w Questions/src/Methods.java
@@ -1,150 +1,142 @@
 import java.util.Scanner;
 public class Methods {
 	int randomarray[] = new int[19];
-	int random = 0, qvalue;
-	static int totalpoint = 0;
-	char rightanswer;
-	String userinput ="";
+	int random = 0, qvalue, qused;
+	static int totalpoints = 0;
+	String userinput, rightanswer;
 	char inputarray[];
+	String questions[], answers[];
+	
 	String takeuserinput(){
 		Scanner userinputscan = new Scanner(System.in);
 			userinput = userinputscan.nextLine();
 		return userinput;
 		}
 	
-	void startupcommand(String userinput){
-		if(userinput.equalsIgnoreCase("help")){
-			System.out.println("Available commands:");
-			System.out.println("1.help : defines commands");
-			System.out.println("2.points : shows total points");
-			System.out.println("3.available subjects : coming soon");
-			System.out.println("4.continue : contiue on to subect choices/");
-			
-		}
-			else if(userinput.equalsIgnoreCase("points")){
-			System.out.println(totalpoint);
-		}
-			else if(userinput.equalsIgnoreCase("available subjects")){
-			System.out.println("Coming soon");
-			}
-			else if(userinput.equalsIgnoreCase("continue"));
-			else{
-				System.out.println("Not a valid command. To see the commands type help");
-			}
+	int subjectchoice(String userinput){
+		if(userinput.equalsIgnoreCase("history")) return 1;
+		else if(userinput.equalsIgnoreCase("points")){System.out.println("Current points: " + totalpoints); return 0;}
+		else if(userinput.equalsIgnoreCase("logic")) return 2;
+		else if(userinput.equalsIgnoreCase("Chemistry")) return 3;
+		else if(userinput.equalsIgnoreCase("Logic")) return 4;
+		else if(userinput.equalsIgnoreCase("Video Games")) return 5;
+		else if(userinput.equalsIgnoreCase("subjects")){System.out.println("History, Chemistry, Logic, Video Games"); return 0;}
+		else{System.out.println("Not a valid subject. To see subjects type subjects."); return 0;}
 	}
 
-	int isValid(char answer) {
-		if(answer == rightanswer) return qvalue;
+	int pointchoice(String userinput){
+		if(userinput.equalsIgnoreCase("100")) return 100;
+		else if(userinput.equalsIgnoreCase("points")){System.out.println("Current points: " + totalpoints); return 0;}
+		else if(userinput.equalsIgnoreCase("250")) return 250;
+		else if(userinput.equalsIgnoreCase("500")) return 500;
+		else if(userinput.equalsIgnoreCase("1000")) return 1000;
+		else if(userinput.equalsIgnoreCase("point values")){System.out.println("100,250,500,1000"); return 0;}
+		else{System.out.println("Not a valid point value. To see point values type point values"); return 0;}
+	}
+	
+	int isCorrect(String answer) {
+		if(answer.equalsIgnoreCase(rightanswer)) return qvalue;
 		else return 0;
 	}
 	int randommeth(){
+		if(qused < 5){
 		do{
 			random = (int)Math.floor(Math.random() * 19);
 		}while (randomarray[random] > 19);
 		randomarray[random] =+ 20;
+		qused += 1;
 		return random;
+		}
+		else return -1;
 	}
+	
+	void setQuestionvalues(String questionrun[], int points, String answerrun[]){
+		for(int i=1; i <questionrun.length; i++) questionrun[i] = questions[i];
+		for(int i=0; i<answerrun.length; i++) answerrun[i] = answers[i];
+		points = qvalue;
+	}
+	
 	void choice(int choice){
 		switch(choice){
 		case 0:
-			System.out.println("Q" + choice + ":...");
-			rightanswer = 'y';
-			qvalue = 100;
+			System.out.println("Q" + choice + questions[choice]);
+			rightanswer = answers[choice];
 			break;
 		case 1:
-			System.out.println("Q" + choice + ":...");
-			rightanswer = 'y';
-			qvalue = 100;
+			System.out.println("Q" + choice + questions[choice]);
+			rightanswer = answers[choice];
 			break;
 		case 2:
-			System.out.println("Q" + choice + ":...");
-			rightanswer = 'y';
-			qvalue = 100;
+			System.out.println("Q" + choice + questions[choice]);
+			rightanswer = answers[choice];
 			break;
 		case 3:
-			System.out.println("Q" + choice + ":...");
-			rightanswer = 'y';
-			qvalue = 100;
+			System.out.println("Q" + choice + questions[choice]);
+			rightanswer = answers[choice];
 			break;
 		case 4:
-			System.out.println("Q" + choice + ":...");
-			rightanswer = 'y';
-			qvalue = 100;
+			System.out.println("Q" + choice + questions[choice]);
+			rightanswer = answers[choice];
 			break;
 		case 5:
-			System.out.println("Q" + choice + ":...");
-			rightanswer = 'y';
-			qvalue = 100;
+			System.out.println("Q" + choice + questions[choice]);
+			rightanswer = answers[choice];
 			break;
 		case 6:
-			System.out.println("Q" + choice + ":...");
-			rightanswer = 'y';
-			qvalue = 100;
+			System.out.println("Q" + choice + questions[choice]);
+			rightanswer = answers[choice];
 			break;
 		case 7:
-			System.out.println("Q" + choice + ":...");
-			rightanswer = 'y';
-			qvalue = 100;
+			System.out.println("Q" + choice + questions[choice]);
+			rightanswer = answers[choice];
 			break;
 		case 8:
-			System.out.println("Q" + choice + ":...");
-			rightanswer = 'y';
-			qvalue = 100;
+			System.out.println("Q" + choice + questions[choice]);
+			rightanswer = answers[choice];
 			break;
 		case 9:
-			System.out.println("Q" + choice + ":...");
-			rightanswer = 'y';
-			qvalue = 100;
+			System.out.println("Q" + choice + questions[choice]);
+			rightanswer = answers[choice];
 			break;
 		case 10:
-			System.out.println("Q" + choice + ":...");
-			rightanswer = 'y';
-			qvalue = 100;
+			System.out.println("Q" + choice + questions[choice]);
+			rightanswer = answers[choice];
 			break;
 		case 11:
-			System.out.println("Q" + choice + ":...");
-			rightanswer = 'y';
-			qvalue = 100;
+			System.out.println("Q" + choice + questions[choice]);
+			rightanswer = answers[choice];
 			break;
 		case 12:
-			System.out.println("Q" + choice + ":...");
-			rightanswer = 'y';
-			qvalue = 100;
+			System.out.println("Q" + choice + questions[choice]);
+			rightanswer = answers[choice];
 			break;
 		case 13:
-			System.out.println("Q" + choice + ":...");
-			rightanswer = 'y';
-			qvalue = 100;
+			System.out.println("Q" + choice + questions[choice]);
+			rightanswer = answers[choice];
 			break;
 		case 14:
-			System.out.println("Q" + choice + ":...");
-			rightanswer = 'y';
-			qvalue = 100;
+			System.out.println("Q" + choice + questions[choice]);
+			rightanswer = answers[choice];
 			break;
 		case 15:
-			System.out.println("Q" + choice + ":...");
-			rightanswer = 'y';
-			qvalue = 100;
+			System.out.println("Q" + choice + questions[choice]);
+			rightanswer = answers[choice];
 			break;
 		case 16:
-			System.out.println("Q" + choice + ":...");
-			rightanswer = 'y';
-			qvalue = 100;
+			System.out.println("Q" + choice + questions[choice]);
+			rightanswer = answers[choice];
 			break;
 		case 17:
-			System.out.println("Q" + choice + ":...");
-			rightanswer = 'y';
-			qvalue = 100;
+			System.out.println("Q" + choice + questions[choice]);
+			rightanswer = answers[choice];
 			break;
 		case 18:
-			System.out.println("Q" + choice + ":...");
-			rightanswer = 'y';
-			qvalue = 100;
+			System.out.println("Q" + choice + questions[choice]);
+			rightanswer = answers[choice];
 			break;
 		case 19:
-			System.out.println("Q" + choice + ":...");
-			rightanswer = 'y';
-			qvalue = 100;
+			System.out.println("Q" + choice + questions[choice]);
+			rightanswer = answers[choice];
 			break;
 	}
 	}
--- First Project w Questions/src/Run.java
+++ First Project w Questions/src/Run.java
@@ -2,16 +2,30 @@
 public class Run {
 	public static void main(String args[])
 			throws java.io.IOException{
-			int random;
-			String userinput = "";
-			Methods question = new Methods();
+			int random, qvalue, identifier = 0;
+			String userinput, subjectpoint;
+			
+			Methods defaultquestion = new Methods();
+			Methods history250 = new Methods();
+			Methods history1000 = new Methods();
+			Methods history750 = new Methods();
+			Methods history500 = new Methods();
+			Methods history100 = new Methods();
+			Methods logic250 = new Methods();
+			
+			System.out.println("To see your points at any given time just type points");
+			
 			do{
-			System.out.println("Please enter a command:");
-			userinput = question.takeuserinput();
-			question.startupcommand(userinput);
-			}while(!userinput.equals("continue"));
-			System.out.println("Pick your subject:(coming soon)");
-			random = question.randommeth();
-			question.choice(random);
-		}
+			System.out.println("Pick your subject:");
+			userinput= defaultquestion.takeuserinput();
+			identifier = defaultquestion.subjectchoice(userinput);
+			}while(identifier == 0);
+			
+			do{
+			System.out.println("Pick your point value:");
+			userinput= defaultquestion.takeuserinput();
+			qvalue = defaultquestion.pointchoice(userinput);
+		}while(qvalue == 0);
+			
+	}
 }
