d654f2019330bce521074f13568bd7075281d08f
Matt64091 <m.martell58@yahoo.com>
2015-10-18 16:07:20 -0400

    Cleaned up the code a bit.

--- Quadratic Solver/src/InputTaker.java
+++ Quadratic Solver/src/InputTaker.java
@@ -1,35 +1,38 @@
 
 public class InputTaker {
-	public static void main(String args[]){	
+	public static void main(String args[])
+		throws java.io.IOException{
 	// A Term
 		char term = 'A';
-		Methods main = new Methods();
+		Methods q = new Methods();
 		System.out.println("Note: Please don't enter fractions, only decimals.");
-		double aterm = main.termnumber(term);
+		double aterm = q.termnumber(term);
 		System.out.println("A term = " + aterm);
 	// B Term
 		term = 'B';
-		double bterm = main.termnumber(term);
+		double bterm = q.termnumber(term);
 		System.out.println("B term = " + bterm);
 	//C Term
 		term = 'C';
-		double cterm = main.termnumber(term);
+		double cterm = q.termnumber(term);
 		System.out.println("C term = " + cterm);
+	//Constructor
+		q.setvalues(aterm, bterm, cterm);
 	//Calculate
-		double discriminant = main.calculatediscriminant(aterm, bterm, cterm);
-		System.out.println("Discriminant = " + discriminant);
+		q.setDiscriminant();
 		boolean check = true;
-		double x1 = main.calculatefinal(check, aterm, bterm, cterm, discriminant);
+		double x1 = q.getX(check);
 		check = false;
-		double x2 = main.calculatefinal(check, aterm, bterm, cterm, discriminant);
+		double x2 = q.getX(check);
 	//Display
-		System.out.println("The equation: "+aterm+"x^2 + "+bterm+"x + "+cterm);
+		System.out.println("The equation: "+aterm+"x^2 + "+bterm+"x + " + cterm);
 		System.out.println("X = "+x1);
 		System.out.println("X = "+x2);
 		System.out.println("If it is equal to NaN, then the roots are imaginary");
 	}
 		
 }
+
 // To do list:
 // Add ability to give fraction answer
 // 		Also, make the fraction answer have +/- sign
--- Quadratic Solver/src/Methods.java
+++ Quadratic Solver/src/Methods.java
@@ -1,5 +1,13 @@
 import java.util.Scanner;
 public class Methods {
+	double a,b,c;
+	double discriminent;
+	//Constructor
+	void setvalues(double a, double b, double c){
+		this.a=a;
+		this.b=b;
+		this.c=c;		
+	}
 	// Get term value
 	double termnumber(char term){
 	Scanner userinputscan = new Scanner(System.in);
@@ -7,21 +15,21 @@ public class Methods {
 	double userinputint = userinputscan.nextInt();
 	return userinputint;
 	}
-	//Calculate the discriminant
-	double calculatediscriminant(double a, double b, double c){
-		double discriminent = b*b-4*a*c;
+	//Calculate the discriminent
+	void setDiscriminant(){
+		discriminent = b*b-4*a*c;
 		discriminent = Math.sqrt(discriminent);
-		return discriminent;
 	}
-	double calculatefinal(boolean check, double a, double b, double c, double discriminant){
+	//Calculate the roots
+	double getX(boolean check){
 		double xterm=0;
 		double bottom = 2*a;
 		if(check){
-			double top = -b + discriminant;
+			double top = -b + discriminent;
 			xterm = top / bottom;
 		}
 		else if(!check){
-			double top = -b - discriminant;
+			double top = -b - discriminent;
 			xterm = top / bottom;
 		}
 		return xterm;
